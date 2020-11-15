84ed6e768ce934c8d7eda47ee5f1e856b7bfd7d9
Andrew <mrbondmustdie@gmail.com>
2020-11-15 09:59:20 +0800

    Cleaned up code

--- view.js
+++ view.js
@@ -105,11 +105,7 @@ const viewByDepartment = async () => {
         .then(function (response) {
 
             if (response.whichDepartment != null) {
-                let query = server.connection.query("SELECT E1.id, E1.first_name, E1.last_name, R.title AS role, R.salary, (CONCAT(E2.first_name, ' ', E2.last_name)) AS manager\
-                FROM employee AS E1\
-                LEFT JOIN employee AS E2 on E1.manager_id = E2.id\
-                LEFT JOIN role AS R ON E1.role_id = R.id\
-                WHERE R.title IN (SELECT title FROM role WHERE department_id=?)", [response.whichDepartment], function (error, response) {
+                let query = server.connection.query("SELECT E1.id, E1.first_name, E1.last_name, R.title AS role, R.salary, (CONCAT(E2.first_name, ' ', E2.last_name)) AS manager FROM employee AS E1 LEFT JOIN employee AS E2 on E1.manager_id = E2.id LEFT JOIN role AS R ON E1.role_id = R.id WHERE R.title IN (SELECT title FROM role WHERE department_id=?)", [response.whichDepartment], function (error, response) {
                     if (error) console.log(`${logSymbols.error} ${error}`);
                     console.table(response);
                     server.mainMenu();
@@ -119,7 +115,6 @@ const viewByDepartment = async () => {
                 console.log(`${logSymbols.error} No Department Selected`);
                 server.mainMenu();
             }
-
         });
 }
 
@@ -138,11 +133,7 @@ const viewByManager = async () => {
         .then(function (response) {
 
             if (response.whichManager != null) {
-                server.connection.query("SELECT E1.id, E1.first_name AS 'first name', E1.last_name AS 'last name', R.title AS role, R.salary\
-                FROM employee AS E1 \
-                LEFT JOIN employee AS E2 on E1.manager_id = E2.id \
-                LEFT JOIN role AS R ON E1.role_id = R.id\
-                WHERE E1.manager_id=?", response.whichManager, function (error, response) {
+                server.connection.query("SELECT E1.id, E1.first_name AS 'first name', E1.last_name AS 'last name', R.title AS role, R.salary FROM employee AS E1 LEFT JOIN employee AS E2 on E1.manager_id = E2.id LEFT JOIN role AS R ON E1.role_id = R.id WHERE E1.manager_id=?", response.whichManager, function (error, response) {
                     if (error) console.log(`${logSymbols.error} ${error}`);
                     console.table(response);
                     server.mainMenu();
@@ -184,12 +175,7 @@ const viewBudget = () => {
 
     console.log("")
 
-    server.connection.query("SELECT  D2.id, D2.name, SUM(R.salary) AS budget\
-    FROM employee AS E1\
-    LEFT JOIN role AS R ON E1.role_id = R.id\
-    LEFT JOIN department AS D2 on R.department_id = D2.id\
-    WHERE R.title IN (SELECT title FROM role)\
-    GROUP BY D2.id", function (error, response) {
+    server.connection.query("SELECT  D2.id, D2.name, SUM(R.salary) AS budget FROM employee AS E1 LEFT JOIN role AS R ON E1.role_id = R.id LEFT JOIN department AS D2 on R.department_id = D2.id WHERE R.title IN (SELECT title FROM role) GROUP BY D2.id", function (error, response) {
         if (error) console.log(`${logSymbols.error} ${error}`);
         console.table(response);
         server.mainMenu();
